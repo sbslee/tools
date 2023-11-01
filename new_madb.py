a7385ec1fb8c33818b4f7195f17a55b62846d31d
Seung-been Lee <sbstevenlee@gmail.com>
2023-11-01 23:01:43 +0900

    Update new_madb.py

--- new_madb.py
+++ new_madb.py
@@ -31,6 +31,12 @@ def parse_ucsc_output(file, assembly):
     df = df.set_index('rs_id')
     return df
 
+def check_data(df):
+    def one_row(r):
+        if set([r.grch37_ref, r.grch37_alt]) != set([r.grch38_ref, r.grch38_alt]):
+            raise ValueError()
+    df.apply(one_row, axis=1)
+
 def prepare_madb_table(grch37_file, grch38_file, output_file):
     df1 = parse_ucsc_output(grch37_file, 'grch37')
     df2 = parse_ucsc_output(grch38_file, 'grch38')
@@ -41,6 +47,7 @@ def prepare_madb_table(grch37_file, grch38_file, output_file):
         raise ValueError()
     df3 = df3.drop(columns='type_y')
     df3 = df3.rename(columns={'type_x': 'type'})
+    check_data(df3)
     df3.to_csv(output_file)
 
 if __name__ == '__main__':
